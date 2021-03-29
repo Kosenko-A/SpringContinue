bbc5116cd3c7dbb1e7097ad5571a0544b76c99c4
Kosenko <Kosenko.2000@yandex.ru>
2021-03-29 17:55:29 +0500

    fixed bugs

--- src/main/java/ru/geekbrains/SpringContinue/repository/ProductRepository.java
+++ src/main/java/ru/geekbrains/SpringContinue/repository/ProductRepository.java
@@ -1,10 +1,11 @@
 package ru.geekbrains.SpringContinue.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.SpringContinue.entity.Product;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
